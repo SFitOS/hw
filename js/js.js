a8da7c7559924367b9c879704e86cfd891a882f5
Roman Kapa <sfitostech@gmail.com>
2020-08-13 19:47:06 +0300

    first commit

---
+++ js/js.js
@@ -0,0 +1,25 @@
+// "use strict";
+
+// alert('hello world!')
+
+// const result = confirm('are uou here?');
+// console.log(result)
+
+// const answer = +prompt('Vam est` 18 let', '18');
+// console.log(answer + 5);
+
+// const answers = [];
+// answers[0] = prompt('Как Вас зовут?');
+// answers[1] = prompt('Сколько Вам лет?');
+// answers[2] = prompt('Укажите Ваш пол');
+
+// document.write(answers);
+
+let incr = 10,
+    decr = 10;
+
+// ++incr;
+// --decr;
+
+console.log(incr++);
+console.log(decr--);
---
+++ js/script.js
@@ -0,0 +1,17 @@
+let number = 5;
+const leftBorderWidth = 1;
+
+number = 10;
+console.log(number);
+
+const obj = {
+    a:50
+};
+
+
+{
+    var result = 50;
+}
+
+console.log(result);
+// с const оно не работает
